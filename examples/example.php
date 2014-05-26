3aeca3133ebe8db144b159dec1190f08b87bf3c7
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-05-26 09:33:42 +0200

    Comment updated

--- examples/example.php
+++ examples/example.php
@@ -11,7 +11,7 @@
 // add your own credentials in this file
 require_once __DIR__ . '/credentials.php';
 
-// required to load
+// required to load (only when not using an autoloader)
 require_once __DIR__ . '/../src/Wunderlist.php';
 
 use JeroenDesloovere\Wunderlist\Wunderlist;
