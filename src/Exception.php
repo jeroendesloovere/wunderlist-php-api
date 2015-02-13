41111dffa50c1aff9984a1aa5f20b997768d11f3
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-02-13 14:50:07 +0100

    Credentials added to Exception

--- src/Exception.php
+++ src/Exception.php
@@ -2,6 +2,13 @@
 
 namespace JeroenDesloovere\Wunderlist;
 
+/*
+ * This file is part of the Wunderlist PHP class from Jeroen Desloovere.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
 /**
  * Wunderlist Exception
  *
