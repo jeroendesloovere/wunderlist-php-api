54a21d9c9b426238832efc35c99e040b95c4d775
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-02-13 14:19:11 +0100

    Example now uses autoload.php from /vendor

--- examples/example.php
+++ examples/example.php
@@ -12,7 +12,7 @@
 require_once __DIR__ . '/credentials.php';
 
 // required to load (only when not using an autoloader)
-require_once __DIR__ . '/../src/Wunderlist.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use JeroenDesloovere\Wunderlist\Wunderlist;
 
